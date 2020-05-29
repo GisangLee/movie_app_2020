0832903c03bd2f6350a67ae84975e25b8eec2706
GisangLee <masterkorea01@naver.com>
2020-05-30 00:29:53 +0900

    Movie lists doesn'y show up to page....

--- src/App.js
+++ src/App.js
@@ -1,31 +1,52 @@
-import React from 'react';
-import PropTypes from "prop-types"
+import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component {
   state = {
-    count: 0
+    isLoading: true,
+    movies: [],
   };
 
-  add = () => {
-    console.log("add")
-    this.setState(current => ({count: current.count + 1}))
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    console.log(movies);
+    this.setState({ movies: movies, isLoading: false });
   };
-  minus = () => {
-    console.log("minus")
-    this.setState(current => ({count: current.count - 1}))
-  };
+
+  componentDidMount() {
+    this.getMovies();
+  }
 
   render() {
+    const { isLoading, movies } = this.state;
     return (
-      <div>
-        <h1>The number is {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+            ))}
+          </div>
+        )}
+      </section>
     );
   }
 }
-
-
 
 export default App;
