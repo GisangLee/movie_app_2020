c836091c42c1217d1f84615f6851f6c840495166
GisangLee <masterkorea01@naver.com>
2020-05-29 23:05:44 +0900

    Learned About State

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import PropTypes from "prop-types"
 
-function App() {
-  return <div className="App" />
+class App extends React.Component {
+  state = {
+    count: 0
+  };
+
+  add = () => {
+    console.log("add")
+    this.setState(current => ({count: current.count + 1}))
+  };
+  minus = () => {
+    console.log("minus")
+    this.setState(current => ({count: current.count - 1}))
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>The number is {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
+
+
 
 export default App;
