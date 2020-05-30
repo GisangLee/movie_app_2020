6fda3a91c1f3e512d8be4d78456f0d74b4a130db
GisangLee <masterkorea01@naver.com>
2020-05-30 12:43:54 +0900

    Finishing all things with api and will starts with CSS

--- src/App.js
+++ src/App.js
@@ -41,6 +41,7 @@ class App extends React.Component {
                 title={movie.title}
                 summary={movie.summary}
                 poster={movie.medium_cover_image}
+                genres={movie.genres}
               />
             ))}
           </div>
--- src/Movie.js
+++ src/Movie.js
@@ -1,12 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
-function Movie({ id, year, title, summary, poster }) {
+function Movie({ year, title, summary, poster, genres }) {
   return (
     <div className="movie">
-      <h3 className="movie__title">{title}</h3>
-      <h5 className="movie__year">{year}</h5>
-      <p className="movie__summary">{summary}</p>
+      <img src={poster} alt={title} title={title} />
+      <div className="movie__data">
+        <h3 className="movie__title">{title}</h3>
+        <h5 className="movie__year">{year}</h5>
+        <ul className="genres">
+          {genres.map((genre, index) => (
+            <li key={index} className="genres__genre">
+              {genre}
+            </li>
+          ))}
+        </ul>
+        <p className="movie__summary">{summary}</p>
+      </div>
     </div>
   );
 }
@@ -17,6 +28,7 @@ Movie.propTypes = {
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Movie;
