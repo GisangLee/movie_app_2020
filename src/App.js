d79489a3278f7941c64397736fe7415ebe9baf7b
GisangLee <masterkorea01@naver.com>
2020-05-30 11:23:17 +0900

    Still Working with Rendering

--- src/App.js
+++ src/App.js
@@ -13,9 +13,11 @@ class App extends React.Component {
       data: {
         data: { movies },
       },
-    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
     console.log(movies);
-    this.setState({ movies: movies, isLoading: false });
   };
 
   componentDidMount() {
@@ -25,26 +27,22 @@ class App extends React.Component {
   render() {
     const { isLoading, movies } = this.state;
     return (
-      <section className="container">
-        {isLoading ? (
-          <div className="loader">
-            <span className="loader__text">Loading...</span>
-          </div>
-        ) : (
-          <div className="movies">
-            {movies.map((movie) => (
-              <Movie
-                key={movie.id}
-                id={movie.id}
-                year={movie.year}
-                title={movie.title}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-              />
-            ))}
-          </div>
-        )}
-      </section>
+      <div>
+        {isLoading
+          ? "Loading..."
+          : movies.map((movie) => {
+              return (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                />
+              );
+            })}
+      </div>
     );
   }
 }
--- src/Movie.js
+++ src/Movie.js
@@ -7,8 +7,8 @@ function Movie(id, year, title, summary, poster) {
 
 Movie.propTypes = {
   id: PropTypes.number.isRequired,
-  title: PropTypes.string.isRequired,
   year: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
 };
